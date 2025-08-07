42c2f426c86c3bd72aa7ed7d6823c52311d2ed53
Roman Njoroge <roman.njoroge@njuguna.com>
2025-08-07 18:06:47 +0300

    feat: Compiling and deploying contract on avalanche fuji testnet

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const KAGE_PRIVATE_KEY = vars.get("KAGE_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    avalancheFuji: {
+      url: "https://avalanche-fuji.drpc.org",
+      accounts: [KAGE_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      avalancheFujiTestnet: ETHERSCAN_API_KEY
+    }
+  }
 };
 
 export default config;
---
+++ contract/process-env.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      BACKEND_ACCOUNT: string;
+    }
+  }
+}
+
+export { };
