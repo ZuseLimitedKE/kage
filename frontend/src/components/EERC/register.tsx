5806fd7f909d3d2d7d027b3ed05c096c021d96b0
Roman Njoroge <roman.njoroge@njuguna.com>
2025-08-08 20:01:28 +0300

    feat: Installing packages needed to generate proofs and generating proof and signals

--- frontend/src/components/EERC/register.tsx
+++ frontend/src/components/EERC/register.tsx
@@ -1,12 +1,62 @@
 import { toast } from "sonner";
 import { Button } from "../ui/button";
+import type RegistrationCircuit from "@/eerc/circuits/registration";
+import { zkit } from "hardhat";
+import { useAccount, useConnectorClient, type Config } from "wagmi";
+import { JsonRpcProvider } from 'ethers'
+import { User } from "@/eerc/user";
 
 export default function RegiserEERCButton() {
-    function handleRegister() {
-        try {   
+    const { address, chainId } = useAccount();
+    const { data: client } = useConnectorClient<Config>({ chainId });
+    async function handleRegister() {
+        try {
+            if (!address || !chainId) {
+                toast.error("Please connect wallet to register");
+                return;
+            }
+
             toast.warning("Beginning register process");
+
+            toast.warning("Getting registration circuit")
+            let registrationCircuit: RegistrationCircuit;
+            const circuit = await zkit.getCircuit("RegistrationCircuit");
+            registrationCircuit = circuit as unknown as RegistrationCircuit;
+
+            toast.warning("Getting account details");
+            if (!client) {
+                toast.error("Could not get client");
+                return;
+            }
+
+            const { account, chain, transport } = client
+            const network = {
+                chainId: chain.id,
+                name: chain.name,
+                ensAddress: chain.contracts?.ensRegistry?.address,
+            }
+
+            toast.warning("Getting user");
+            const provider = new JsonRpcProvider(transport.url, network);
+            const signer = await provider.getSigner(account.address);
+            const user = new User(signer);
+
+            toast.warning("Getting proof and signals");
+            const registrationHash = user.genRegistrationHash(BigInt(chainId));
+            const input = {
+                SenderPrivateKey: user.formattedPrivateKey,
+                SenderPublicKey: user.publicKey,
+                SenderAddress: BigInt(user.signer.address),
+                ChainID: chainId,
+                RegistrationHash: registrationHash,
+            };
+            const proof = await registrationCircuit.generateProof(input);
+            const calldata = await registrationCircuit.generateCalldata(proof);
+
+            console.log(proof, calldata);
+
             toast.success("Registered succesfully");
-        } catch(err) {
+        } catch (err) {
             console.error("Error registering", err);
         }
     }
