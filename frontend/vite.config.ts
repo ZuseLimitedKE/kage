d9946751cffc1f301c110ad779724c636763bb40
mbeka02 <antonymbeka@gmail.com>
2025-08-08 20:58:21 +0300

    modified vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      assert: "assert",
+      buffer: "buffer",
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      util: "util",
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer", "process"],
+  },
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
@@ -15,11 +31,6 @@ export default defineConfig({
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-})
+});
